81ced1711690c393c61beac8b818b13eec5b482c
Utkarsh S. <utkarshsolanki776@gmail.com>
2026-08-02 22:26:51 +0530

    Implement HTTP request body parsing

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,13 @@
 from request import Request
 def parse_request(data):
+    data = data.decode('utf-8')
     lines = data.split("\r\n")
+    body = ""
+    for index, line in enumerate(lines):
+        print(index, repr(line))
+        if line == "":
+         body = "\r\n".join(lines[index + 1:])
+         break
     request_line = lines[0]
     method, path, version = request_line.split()
     headers = {}
@@ -10,4 +17,4 @@ def parse_request(data):
         header, separator, value = line.partition(": ")
         if separator:
             headers[header] = value
-    return Request(method, path, version, headers)
+    return Request(method, path, version, headers, body)
