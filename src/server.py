c737875e1c9bac743752da09c546cd18cb5f4f44
Utkarsh S. <utkarshsolanki776@gmail.com>
2026-08-02 06:27:08 +0530

    Refactor parser into separate module

--- src/server.py
+++ src/server.py
@@ -1,35 +1,21 @@
 import socket
-
+from parser import parse_request
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 8080))
-
 headers = {}
 
 try:
     server_socket.listen(1)
     print('Server is listening on port 8080...')
-
     conn, addr = server_socket.accept()
     print('Connection from:', addr)
-
     data = conn.recv(1024)
     data = data.decode('utf-8')
-
-    lines = data.split("\r\n")
-
-    request_line = lines[0]
-
-    method, path, version = request_line.split()
-
-    headers['Method'] = method
-    headers['Path'] = path
-    headers['Version'] = version
-
-    for line in lines[1:]:
-        header, separator, value = line.partition(": ")
-        headers[header] = value
-        if header and value:
-            print(f"{header}: {value}")
-
+    print('Received data:', data)
+    request = parse_request(data)
+    print('Method:', request.method)
+    print('Path:', request.path)
+    print('Version:', request.version)
+    print('Headers:', request.headers)
 except Exception as e:
     print('Error:', e)
