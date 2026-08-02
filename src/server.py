3569bb9bb91702b544ba8553591fbc30cd6bf109
Utkarsh S. <utkarshsolanki776@gmail.com>
2026-08-02 22:26:54 +0530

    Extend Request object with body support

--- src/server.py
+++ src/server.py
@@ -2,20 +2,16 @@ import socket
 from parser import parse_request
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 8080))
-headers = {}
-
+server_socket.listen(1)
+print("Proxy server is running on http://localhost:8080")
 try:
-    server_socket.listen(1)
-    print('Server is listening on port 8080...')
-    conn, addr = server_socket.accept()
-    print('Connection from:', addr)
-    data = conn.recv(1024)
-    data = data.decode('utf-8')
-    print('Received data:', data)
-    request = parse_request(data)
-    print('Method:', request.method)
-    print('Path:', request.path)
-    print('Version:', request.version)
-    print('Headers:', request.headers)
-except Exception as e:
-    print('Error:', e)
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection from {client_address}")
+    request_data = client_socket.recv(1024)
+    request = parse_request(request_data)
+    print("request body")
+    print(request.body)
+    print(f"Received request: {request.method} {request.path} {request.version} {request.headers} {request.body}")
+finally:
+    client_socket.close()
+    server_socket.close()   
